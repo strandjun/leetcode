59f98bb218de6326c990ff6b14e8395053d4e51a
strandjun <strand.jun@gmail.com>
2017-07-12 17:40:00 +0800

    modify

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -1,68 +1,79 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param  {string}  inputStr
+ * @return {string}  [验证结果是否正确]
  * 
  * 26进制+1
  * 
  */
 
-// 26进制+1
-function succ(inputStr) {
 
-    if(inputStr.trim() == ''){
+/**
+ * 递归处理最后一位数字或字母+1
+ * @param  {[string]} str          [原字符串]
+ * @param  {[string]} type         [最后一位类型]
+ * @type   {0:大写; 1:小写; 2:数字;} [默认为0]
+ * @return {[string]}              [组合好的数组]
+ */
+function lastBitAdd(str, type) {
+
+    let len = str.length,
+        newStr = '';
+    type = type ? type : '0';
+
+    // 如果前面没有，则根据type进位
+    if (len == 0) {
+        return type == '0' ? 'A' : (type == '1' ? 'a' : '0');
+    }
+
+    // 判断是否进位,并重新确定type
+    let lastStr = str[len - 1],
+        lastNum;
+    if (isNaN(lastStr)) {
+        lastNum = lastStr.charCodeAt();
+        type = lastNum > 90 ? '1' : '0';
+    } else {
+        lastNum = lastStr;
+        type = '2';
+    }
+
+    if (lastNum == 90 || lastNum == 122 || lastNum == 9) {
+        let temp = type == '0' ? 'A' : (type == '1' ? 'a' : '0');
+        return lastBitAdd(str.substr(0, len - 1), type) + temp;
+    } else {
+        if (type == '2') {
+            lastNum++;
+            newStr = lastNum.toString();
+        } else {
+            newStr = String.fromCharCode(++lastNum);
+        }
+        return str.substr(0, len - 1) + newStr;
+    }
+}
+
+// 26进制+1调用
+function succ(inputStr) {
+    if (inputStr.trim() == '') {
         return inputStr;
     }
-
-    let lastNum,newStr='';
-
-    // 递归处理最后一位数字+1
-    var numAdd = function(inputStr){
-        let inputStrlen = inputStr.length;
-        if(inputStrlen == 0){
-            newStr = 'A' + newStr;
-            return;
-        }
-        let str = inputStr[inputStrlen-1];
-        if(isNaN(str)){
-            lastNum = str.charCodeAt();
-        }else{
-            lastNum = str;
-        }
-        if(lastNum == 90){
-            newStr = 'A' + newStr;
-            numAdd(inputStr.substr(0,inputStrlen-1));
-        }else if(lastNum == 122){
-            newStr = 'a' + newStr;
-            numAdd(inputStr.substr(0,inputStrlen-1));
-        }else if(lastNum == 9){
-            newStr = '0' + newStr;
-            numAdd(inputStr.substr(0,inputStrlen-1));
-        }else{
-            newStr = String.fromCharCode(++lastNum) + newStr;
-            if(inputStrlen >= 2){
-                newStr = inputStr.substr(0,inputStrlen-1) + newStr;
-            }
-        }
-    }
-    numAdd(inputStr);
-
-    return newStr;
-
+    return lastBitAdd(inputStr);
 }
 
-function assertEq(a, b) {
-    if( a == b) {
-        console.log(a + "==" + b)
+// 结果呈现
+function assertEq(a, b, s) {
+    if (a == b) {
+        console.log(s + a + "==" + b)
     } else {
-        console.error(a + "!=" + b)
+        console.error(s + a + "!=" + b)
     }
 }
 
-assertEq(succ("AA"), "AB");
-assertEq(succ("AZ"), "BA");
-assertEq(succ("AZZ"), "BAA");
-assertEq(succ("Z"), "AA");
-assertEq(succ(" "), " ");
-assertEq(succ("Aa"), "Ab");
-assertEq(succ("Az"), "Ba");
-assertEq(succ("A9"), "B0");
+assertEq(succ("AA"),  "AB",  '1:AA   ');
+assertEq(succ("AZ"),  "BA",  '2:AZ   ');
+assertEq(succ("AZZ"), "BAA", '3:AZZ  ');
+assertEq(succ("Z"),   "AA",  '4:Z    ');
+assertEq(succ(" "),   " ",   '5:     ');
+assertEq(succ("Aa"),  "Ab",  '6:Aa   ');
+assertEq(succ("Az"),  "Ba",  '7:Az   ');
+assertEq(succ("A9"),  "B0",  '8:A9   ');
+assertEq(succ("A99"), "B00", '9:A99  ');
+assertEq(succ("A59"), "A60", '10:A59 ');
