9aa8e6718b7995778dcd98554113fa9ab2e65fc2
tusi <tusi@meili-inc.com>
2018-07-23 23:55:34 +0800

    pramp 2

---
+++ Pramp_ShiftedArraySearch.js
@@ -0,0 +1,68 @@
+/**
+QUESTION
+Shifted Array Search
+A sorted array of distinct integers shiftArr is shifted to the left by an unknown offset and you don’t have a pre-shifted copy of it. For instance, the sequence 1, 2, 3, 4, 5 becomes 3, 4, 5, 1, 2, after shifting it twice to the left.
+
+Given shiftArr and an integer num, implement a function shiftedArrSearch that finds and returns the index of num in shiftArr. 
+If numisn’t in shiftArr, return -1. 
+Assume that the offset doesn’t equal to 0 (i.e. assume the array is shifted at least once) or to arr.length - 1 (i.e. assume the shifted array isn’t fully reversed).
+Explain your solution and analyze its time and space complexities.
+
+Example:
+input:  shiftArr = [9, 12, 17, 2, 4, 5], num = 2 # shiftArr is the
+                                                 # outcome of shifting
+                                                 # [2, 4, 5, 9, 12, 17]
+                                                 # three times to the left
+output: 3 # since it’s the index of 2 in arr
+
+Constraints:
+* [time limit] 5000ms
+* [input] array.integer arr
+* [input] integer num
+* [output] integer
+*/
+
+// shiftedArrSearch
+function shiftedArrSearch(shiftArr, num) {
+    let pivot = findPivotPoint(shiftArr);
+    if (pivot == 0 || num < shiftArr[0]) {
+        return binarySearch(shiftArr, pivot, shiftArr.length - 1, num);
+    }
+    return binarySearch(shiftArr, 0, pivot - 1, num);
+}
+
+function findPivotPoint(arr) {
+    let begin = 0,
+        end = arr.length - 1,
+        mid;
+    while (begin <= end) {
+        mid = Math.floor((end + begin) / 2);
+        if (end == 0 || arr[mid - 1] > arr[mid]) {
+            return mid;
+        } else if (arr[0] < arr[mid]) {
+            begin = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return false;
+}
+
+function binarySearch(arr, begin, end, num) {
+    let mid;
+    while (begin <= end) {
+        mid = Math.floor((end + begin) / 2);
+        if (arr[mid] < num) {
+            begin = mid + 1;
+        } else if (arr[mid] == num) {
+            return mid;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+var shiftArr = [9, 12, 17, 2, 4, 5],
+    num = 2;
+console.log(shiftedArrSearch(shiftArr, num));
---
+++ pramp_SentenceReverse.js
@@ -0,0 +1,119 @@
+/**
+Sentence Reverse
+You are given an array of characters arr that consists of sequences of characters separated by space characters. Each space-delimited sequence of characters defines a word.
+
+Implement a function reverseWords that reverses the order of the words in the array in the most efficient manner.
+
+Explain your solution and analyze its time and space complexities.
+*/
+
+/** Example:
+input:  arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
+                'm', 'a', 'k', 'e', 's', '  ',
+                'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ]
+
+output: [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ',
+          'm', 'a', 'k', 'e', 's', '  ',
+          'p', 'e', 'r', 'f', 'e', 'c', 't' ]
+*/
+
+/**
+Constraints:
+
+[time limit] 5000ms
+
+[input] array.character arr
+
+0 ≤ arr.length ≤ 100
+[output] array.character
+*/
+
+// tu initial version
+// function reverseWords(arr) {
+//     let arrBegin=0, newArr=[], k=0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === " ") {
+//             newArr[k] = arr.slice(arrBegin,i);
+//             arrBegin = i+1;
+//             k++;
+//         }
+//     }
+//     newArr.push(arr.slice(arrBegin,arr.length))
+//     // 不知道reverse、concat里做了什么，不知道空间、时间复杂度，所以最好不用…
+//     newArr.reverse();
+//     console.log(newArr.join(' | '));
+//     newArr = Array.prototype.concat.apply([], newArr);
+//     // newArr = [].concat(newArr);
+//     return newArr;
+// }
+
+// update version
+// function reverseWords(arr) {
+//     let wordNum = [],
+//         arrNew = [],
+//         arrLen = arr.length;
+//     // wordNum.push(-1);
+//     for (let i = 0; i < arrLen; i++) {
+//         if (arr[i] === " ") {
+//             wordNum.push(i);
+//         }
+//     }
+//     wordNum.push(arr.length);
+//     // console.log(wordNum, arr.length);
+//     for (let j = wordNum.length - 1; j >= 0; j--) {
+//         if (j > 0) {
+//             arrNew = arrNew.concat(arr.slice(wordNum[j - 1] + 1, wordNum[j]), [" "]);
+//         } else if (j === 0) {
+//             arrNew = arrNew.concat(arr.slice(0, wordNum[j]));
+//         }
+//     }
+//     return arrNew;
+// }
+
+// yuri version
+function reverseWords(arr) {
+    var indexArr = [];
+
+    var start = 0,
+        end = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const char = arr[i];
+        if (char === " ") {
+            end = i;
+            indexArr.push([start, end]);
+            start = i + 1;
+        }
+    }
+    indexArr.push([start, arr.length]);
+
+    var result = [];
+    for (let i = indexArr.length - 1; i >= 0; i--) {
+        const [start, end] = indexArr[i];
+
+        for (let index = start; index < end; index++) {
+            result.push(arr[index]);
+        }
+        if (i != 0) {
+            result.push(" ");
+        }
+    }
+    return result;
+}
+
+let arr = [
+    "p", "e", "r", "f", "e", "c", "t", " ",
+    "m", "a", "k", "e", "s", " ",
+    "p", "r", "a", "c", "t", "i", "c", "e"
+];
+console.log(reverseWords(arr));
+
+/**
+时间复杂度只有 N，在时间复杂度上是最优解了
+
+平时说的 “几个for循环” 是指嵌套的，会导致时间复杂度加倍
+但这里第一第二个for是并列的，时间复杂度还是N
+第二个for里边虽然还有一个for循环，但要注意外部这个for循环是循环一个小数组，有几个单词才会循环几次，和字符数N相比可以忽略不计，要计入的只有里边的循环，而那个循环起点终点都不重合，也就是说也只遍历了一次N
+所以最终还是N
+
+所以时间、空间复杂度都是N
+*/
