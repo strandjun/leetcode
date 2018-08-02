0e540b03c1b23c902dc273808b33529f53d989dc
June <strand.jun@gmail.com>
2018-08-02 23:16:53 +0800

    update

--- 872.Leaf-SimilarTrees.js
+++ 872.Leaf-SimilarTrees.js
@@ -15,10 +15,10 @@
  * @return {boolean}
  */
 
-var tmpArr = [];
+var tmpStr = "";
 var findLeaf = function(node) {
     if (node.left == null && node.right == null) {
-        tmpArr.push(node.val);
+        tmpStr += " " + node.val;
     }
     if (node.left != null) {
         findLeaf(node.left);
@@ -26,14 +26,15 @@ var findLeaf = function(node) {
     if (node.right != null) {
         findLeaf(node.right);
     }
+    return tmpStr;
 };
 
 var leafSimilar = function(root1, root2) {
-    findLeaf(root1);
-    let tmp1 = tmpArr.join(",");
-    tmpArr = [];
-    findLeaf(root2);
-    let tmp2 = tmpArr.join(",");
+    var tmp1, tmp2;
+    tmp1 = findLeaf(root1);
+    tmpStr = "";
+    tmp2 = findLeaf(root2);
+
     if (tmp1 === tmp2) {
         return true;
     } else {
@@ -42,11 +43,56 @@ var leafSimilar = function(root1, root2) {
 };
 
 // var root1 = [1],
-//     root2 = [1];    
+//     root2 = [1];
 // output: true;
 
-var root1 = [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4],
-    root2 = [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8];
+// var root1 = [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4],
+//     root2 = [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8];
 // output: true;
+
+
+// 模拟出的TreeNode Object, 可总说我这个用例过不去… 炸裂 单独测明明ok的
+// var root1 = { val: 1, left: null, right: null },
+//     root2 = { val: 1, left: null, right: null };
+
+// 模拟出的TreeNode Object
+var root1 = {
+        val: 3,
+        left: {
+            val: 5,
+            left: { val: 6, left: null, right: null },
+            right: {
+                val: 2,
+                left: { val: 7, left: null, right: null },
+                right: { val: 4, left: null, right: null }
+            }
+        },
+        right: {
+            val: 1,
+            left: { val: 9, left: null, right: null },
+            right: { val: 8, left: null, right: null }
+        }
+    },
+    root2 = {
+        val: 3,
+        left: {
+            val: 5,
+            left: { val: 6, left: null, right: null },
+            right: {
+                val: 7,
+                left: null,
+                right: null
+            }
+        },
+        right: {
+            val: 1,
+            left: { val: 4, left: null, right: null },
+            right: {
+                val: 2,
+                left: { val: 9, left: null, right: null },
+                right: { val: 8, left: null, right: null }
+            }
+        }
+    };
 
 console.log(leafSimilar(root1, root2));
